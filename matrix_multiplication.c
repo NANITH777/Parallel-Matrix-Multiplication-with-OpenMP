21afd2e35b851975498bce1d8f4d035ea369622e
NANITH777 <nanifulchany@gmail.com>
2024-12-17 14:55:15 +0300

    Control and Open Files

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -6,5 +6,20 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 4) {
+        printf("Error: Incorrect number of arguments.\n");  
+        printf("Usage: %s <matrix_a.txt> <matrix_b.txt> <sonuclar.txt>\n", argv[0]);
+        printf("Please provide three files:\n");
+        return 1;
+    }
+
+    // Open input files
+    FILE *fa = fopen(argv[1], "r");
+    FILE *fb = fopen(argv[2], "r");
+    if (fa == NULL || fb == NULL) {
+        printf("Error opening input files.\n");
+        return 1;
+    }
+
     return 0;
 }
